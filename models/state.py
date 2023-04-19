14488445f711af8491225b8040b621f6601a5cb0
Hope_Davids <hopedavids@gmail.com>
2023-04-19 11:30:04 +0000

    modified states class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column('name', String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute that returns the list of City instances
+            with state_id equals to the current State.id"""
+            from models import storage
+            city_list = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
