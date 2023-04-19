4f1b033364090696ce3b57aba65f489c841b4f7b
Hope_Davids <hopedavids@gmail.com>
2023-04-19 12:17:26 +0000

    added the city class to a database

--- models/city.py
+++ models/city.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String, CHAR, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+"""This is the city class"""
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    
-    __tablename__ = 'city'
-
-    name = Column('name', String(128), nullable=False)
-    state_id = Column('state_id', String(60), ForeignKey('state_id'), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """instantiates a new city"""
-        super().__init__(*args, **kwargs)
-
+class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    # initialize class for file/db storage type
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', cascade='all, delete', backref='cities')
