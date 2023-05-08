61d0f21bd324063e2c522c602500f95ba6c6199d
Hope_Davids <hopedavids@gmail.com>
2023-05-08 09:38:49 +0000

    setuping web-static nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,12 @@
 #!/usr/bin/env bash
-# This script contains the deployment script for webserver 
-sudo apt-get update
-sudo apt-get install -y nginx
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-#Define variables
-link_name="/data/web_static/current"
-target_path="/data/web_static/releases/test"
-# Check if the symbolic link already exists
-if [ -L "$link_name" ]; then
-  rm "$link_name"   # delete it if it exists
-fi
-
-# Create the symbolic link
-ln -s "$target_path" "$link_name"
+# Deploy static AirBnB webserver suing this bash script
+do apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-
-#Define variables
-config_file="/etc/nginx/sites-available/default"
-nginx_service="nginx"
-
-# Update the Nginx configuration file
-sudo sed -i 's#^\(\s*\)location /hbnb_static/ {\(.*\)$#\1location /hbnb_static/ {\n\1    alias /data/web_static/current/;\n\1    \2#' $config_file
-
-# Restart Nginx
-sudo systemctl restart $nginx_service
+NEW_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "38i $NEW_STRING" /etc/nginx/sites-available/default
+sudo service nginx restart
