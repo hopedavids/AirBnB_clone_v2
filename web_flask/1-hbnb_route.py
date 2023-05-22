b12808cfdfbcbd17689ce2c4bd9703f2019c4bac
Hope_Davids <hopedavids@gmail.com>
2023-05-22 06:52:44 +0000

    route the hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This script starts the flask app and listern on 0.0.0.0:5000
+    on / route
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Returns some text. """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns hbnb """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
