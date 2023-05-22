f56c809b701d59afd2e32cb49f2f18b4ff72826c
Hope_Davids <hopedavids@gmail.com>
2023-05-22 06:48:43 +0000

    route hello world

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-from flask import Flask
-
 """ This script starts the flask app and listern on 0.0.0.0:5000
     on / route
 """
+
+from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Returns some text. """
     return "Hello HBNB!\n"
 
 
