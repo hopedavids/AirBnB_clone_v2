931c3b7acce79c8cb237ebdfa03f69eb14a8e1ee
Hope_Davids <hopedavids@gmail.com>
2023-05-22 07:13:51 +0000

    add route to python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" This script starts the flask app and listern on 0.0.0.0:5000
+    on / route
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Returns some text. """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/')
+@app.route('/python/(<text>)')
+def python_magic(text='is cool'):
+    """displays python follow by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
